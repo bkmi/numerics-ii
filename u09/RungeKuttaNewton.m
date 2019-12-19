1306c437dfb316f9b2d8b19ea0c8afa3220c32e8
bkmi <benmiller1315@gmail.com>
2019-12-19 16:01:22 +0100

    working on u09

---
+++ u09/RungeKuttaNewton.m
@@ -0,0 +1,28 @@
+% function [x, t, k] = RungeKuttaNewton(M, x0, I, tau, b, A)
+function [x, t] = RungeKuttaNewton(f, Df, x0, I, tau, b, A, TOL)
+
+% reformulation:
+% k_i = f(g_i) ,  i=1,...s
+% z_i = g_i - x , i=1,...,s
+% Z = [z_1;...;z_s].
+
+b = b(:);
+d = length(x0);
+s = length(b);
+t = I(1):tau:I(2);
+N = length(t);
+x = zeros(d, N);
+k = zeros(d,s,N);
+x(:,1) = x0;
+
+for i=2:N
+    DF = kron(A, Df(x(:,i-1)));
+    DG0 = eye(s*d) - tau*DF;
+    
+    X = kron(ones(s,1), x(:,i-1));
+    G0 = tau*DF*X;
+    Z = DG0\G0;
+    k(:,:,i) = M*reshape(X+Z, d, s);
+    x(:,i) = x(:,i-1) + tau*k(:,:,i)*b;
+end
+
