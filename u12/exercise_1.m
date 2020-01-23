c7a3cf929f4d7860d8da02ba215efb15192d0db5
bkmi <benmiller1315@gmail.com>
2020-01-23 12:26:58 +0100

    well it isnt right but it runs

--- u12/exercise_1.m
+++ u12/exercise_1.m
@@ -5,42 +5,51 @@ I = [0.0, 20.0];
 p0 = 0.;
 q0 = 0.;
 taus = [1, 0.1];
+tau_names = {"1", "0p1"};
 
 for j=1:2
   figure;
-  [p, q, t] = stoermer_verlet(m, g, r0, p0, q0, I, taus(j))
-  q_mod = mod(q, 360.)
-  plot(t, q_mod)
-  title(strcat("Time evolution, Tau = ", num2str(taus(j))))
-  xlabel("time")
-  ylabel("q")
+  [p, q, t] = stoermer_verlet(m, g, r0, p0, q0, I, taus(j));
+  q_mod = mod(q, 360.);
+  plot(t, q_mod);
+  title(strcat("Time evolution, Tau = ", num2str(taus(j))));
+  xlabel("time");
+  ylabel("q");
+  h = gcf;
+  saveas(h, strcat("time_evo_tau_", tau_names{j}), "png")
 endfor
 
 for j=1:2
   figure;
-  plot(q, p)
-  title(strcat("Phase Space evolution, Tau = ", num2str(taus(j))))
-  xlabel("q")
-  ylabel("p")
+  plot(q, p);
+  title(strcat("Phase Space evolution, Tau = ", num2str(taus(j))));
+  xlabel("q");
+  ylabel("p");
+  h = gcf;
+  saveas(h, strcat("phase_space_evo_", tau_names{j}), "png")
 endfor
 
-x = r0 * cos(q)
-y = r0 * sin(q)
+x = r0 * cos(q);
+y = r0 * sin(q);
 
 for j=1:2
   figure;
-  plot(x, y)
-  title(strcat("Euclidean Coordinates, Tau = ", num2str(taus(j))))
-  xlabel("x")
-  ylabel("y")
+  plot(x, y);
+  title(strcat("Euclidean Coordinates, Tau = ", num2str(taus(j))));
+  xlabel("x");
+  ylabel("y");
+  h = gcf;
+  saveas(h, strcat("euc_evo_", tau_names{j}), "png")
 endfor
 
-H = q + p
+H = q + p;
 
 for j=1:2
   figure;
-  plot(t, H)
-  title(strcat("Hamiltonian in time, Tau = ", num2str(taus(j))))
-  xlabel("t")
-  ylabel("H")
+  plot(t, H);
+  title(strcat("Hamiltonian in time, Tau = ", num2str(taus(j))));
+  xlabel("t");
+  ylabel("H");
+  h = gcf;
+  saveas(h, strcat("hamiltonian_evo_", tau_names{j}), "png")
 endfor
--- u12/stoermer_verlet.m
+++ u12/stoermer_verlet.m
@@ -2,15 +2,15 @@ function [p, q, t] = stoermer_verlet (m, g, r0, p0, q0, I, tau)
 
   d = length(p0);
   t = I(1):tau:I(2);
-  N = length(t);
+  N = length(t) + 1;
   dt = (I(2)-I(1))/N;
   p = zeros(d, N);
   p(:,1) = p0;
   q = zeros(d, N);
   q(:,1) = q0;
   
-  f_dq = @(pp) pp/m;
-  f_dp = @(qq) -m * g / r0 * cos(qq);
+  f_dq = @(pp) pp / m;
+  f_dp = @(qq) -(m * g / r0) * cos(qq);
   step = @(q1, q2, dpp) 2 * q2 - q1 + dpp * dt^2;
   velo = @(q1, q2) (q2 - q1) / (2 * dt);
   
@@ -18,12 +18,15 @@ function [p, q, t] = stoermer_verlet (m, g, r0, p0, q0, I, tau)
     dq = f_dq(p(:, i-1));
     dp = f_dp(q(:, i-1));
     if i == 2
-      q(:, i) = q(:, i-1) + dq * dt + dp * dt^2 * 0.5;
+      q(:, i) = q(:, i-1) + dq * dt + (dp / m) * dt^2 * 0.5;
     else
       q(:, i) = step(q(:, i-2), q(:, i-1), p(:, i-1));
+      p(:, i-1) = velo(q(:, i-2), q(:, i)); 
     endif
-    p(:, i) = velo(q(:, i-1), q(:, i)); 
+    % p(:, i) = dq;
   endfor
+  q = q(:, 1:N-1)
+  p = p(:, 1:N-1)
   
   
   
