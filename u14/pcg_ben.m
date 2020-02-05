c7ac6dba16a3a481c1ab3677af5e832cfff8c37d
bkmi <benmiller1315@gmail.com>
2020-02-05 16:08:39 +0100

    got them working

--- u14/pcg_ben.m
+++ u14/pcg_ben.m
@@ -1,23 +1,27 @@
-function [u, uk] = cg(A, b, u0, tol, uexact)
+function [u, uk] = pcg_ben(A, b, u0, tol, uexact, pre)
     n = length(b);
     uk = zeros(n, n); % Theoretical max iterations.
     uk(:, 1) = u0;
     
     r0 = b - A * u0;
-    e0 = r0;
+    z0 = pre(r0);
+    e0 = z0;
     k = 0;
     
     u = u0;
     r = r0;
+    z = z0;
     e = e0;
-    r_norm_old = r' * r;
+    r_norm_old = r' * z0;
     while sqrt((A * (u - uexact))' * (u - uexact)) >= tol
       Ae = A * e;
       alpha = r_norm_old / (Ae' * e);
       
       u = u + alpha * e;
       r = r - alpha * Ae;
-      r_norm_new = r' * r;
+      z = pre(r);
+      
+      r_norm_new = z' * r;
       beta = r_norm_new / r_norm_old;
       e = r + beta * e;
       r_norm_old = r_norm_new;
--- u14/test.m
+++ u14/test.m
@@ -7,3 +7,9 @@ tol = 0.01;
 uexact = A \ b;
 
 [u, uk] = cg(A, b, u0, tol, uexact);
+
+% B = [1, 0; 0, 1];
+B = [1, 0.1; 0.1, 1];
+Binv = inv(B);
+pre = @(x)Binv * x;
+[u_pre, uk_pre] = pcg_ben(A, b, u0, tol, uexact, pre);
